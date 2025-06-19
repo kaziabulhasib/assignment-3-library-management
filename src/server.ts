fbe67e61a00a412e917b0c15c6d52be93c34b65b
kaziabulhasib <hasibkazi420@gmail.com>
2025-06-19 22:43:44 +0530

    db connection with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 let server: Server;
 
 const PORT = 5000;
-
+const uri = process.env.URI as string;
 async function main() {
-  server = app.listen(PORT, () => {
-    console.log(`server is running on port : ${PORT}`);
-  });
+  try {
+    await mongoose.connect(uri);
+    console.log("server connect to db with help of mongoose");
+    server = app.listen(PORT, () => {
+      console.log(`server is running on port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
