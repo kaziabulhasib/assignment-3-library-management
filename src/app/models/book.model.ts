4280ffc8dc54488cfe373a60f0be1d4d8a40159d
kaziabulhasib <hasibkazi420@gmail.com>
2025-06-21 09:56:53 +0530

    borrow interface & borrow model

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 import { IBook } from "../interfaces/book.interface";
 
-const bookSchema = new mongoose.Schema<IBook>({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: {
-    type: String,
-    required: true,
-    enum: [
-      "FICTION",
-      "NON_FICTION",
-      "SCIENCE",
-      "HISTORY",
-      "BIOGRAPHY",
-      "FANTASY",
-    ],
+const bookSchema = new mongoose.Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      required: true,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String },
+    copies: { type: Number, required: true, min: 0 },
+    available: { type: Boolean, default: true },
   },
-  isbn: { type: String, required: true, unique: true },
-  description: { type: String },
-  copies: { type: Number, required: true, min: 0 },
-  available: { type: Boolean, default: true },
-});
+  { timestamps: true }
+);
 
 export const Book = mongoose.model("Book", bookSchema);
