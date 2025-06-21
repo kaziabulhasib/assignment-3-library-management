eecaf6b06098a8c7727d8b1f8273c2b2aa18094c
kaziabulhasib <hasibkazi420@gmail.com>
2025-06-21 11:26:11 +0530

    update & delete a book

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -50,7 +50,7 @@ booksRoutes.get("/", async (req: Request, res: Response) => {
   }
 });
 
-//  get book by book id
+//  get single  book by book id
 
 booksRoutes.get("/:bookId", async (req: Request, res: Response) => {
   const bookId = req.params.bookId;
@@ -59,6 +59,33 @@ booksRoutes.get("/:bookId", async (req: Request, res: Response) => {
   res.status(201).json({
     success: true,
     message: "single book",
+    book,
+  });
+});
+
+// update a book
+
+booksRoutes.put("/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+  const updatedBody = req.body;
+  const book = await Book.findByIdAndUpdate(bookId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "note updated",
+    book,
+  });
+});
+
+// delete a book
+booksRoutes.delete("/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+
+  const book = await Book.findByIdAndDelete(bookId);
+
+  res.status(201).json({
+    success: true,
+    message: "book deleted",
     book,
   });
 });
